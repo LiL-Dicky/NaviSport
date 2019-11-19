0a71c4ae25aa9123f912ad818f367be3a7bc43fc
Kapitanov Nikita 750503 <kapnik3750@gmail.com>
2019-11-19 20:14:30 +0300

    Added transition to main window

--- NaviSport/app/src/main/java/com/example/navisport/AddPoint.java
+++ NaviSport/app/src/main/java/com/example/navisport/AddPoint.java
@@ -2,6 +2,8 @@ package com.example.navisport;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddPoint extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class AddPoint extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_point);
+
+        Button ended = findViewById(R.id.Ended);
+        ended.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddPoint.this.finish();
+            }
+        });
     }
 }
